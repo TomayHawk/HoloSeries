5f4ef2e7b35057b9a34fe5cb62302bc3cfcc15a2
PixelatedMike <michaeldk218@gmail.com>
2024-06-23 16:29:02 -0400

    update pause button

--- scripts/user_interfaces_scripts/options.gd
+++ scripts/user_interfaces_scripts/options.gd
@@ -1,7 +1,13 @@
 extends CanvasLayer
 
 func _on_escape_pressed():
-    if !GlobalSettings.in_combat:
-        get_tree().paused = true
-        GlobalSettings.game_paused = true
-        GlobalSettings.in_settings = true
+	if !GlobalSettings.game_paused:
+		get_tree().paused = true
+		GlobalSettings.game_paused = true
+		GlobalSettings.in_settings = true
+		$MarginContainer/MarginContainer2/HBoxContainer/Escape.text = "▶"
+	else:
+		get_tree().paused = false
+		GlobalSettings.game_paused = false
+		GlobalSettings.in_settings = false
+		$MarginContainer/MarginContainer2/HBoxContainer/Escape.text = "II"
