2488e04a3d0a2908bc087cb5d4b43bb362fdc9e8
Shuail135 <sumyanwan@gmail.com>
2024-06-28 19:41:46 -0400

    Fireball (read description pls)
    Now it will detect enemy and shoot the direction in straight line(however if the enemy move, it might miss), if no enemy, it will shoot at player walking direction, if player in idle, it will shoot in player attack direction(?)

--- scripts/entities_scripts/abilities_scripts/fireball.gd
+++ scripts/entities_scripts/abilities_scripts/fireball.gd
@@ -3,11 +3,25 @@ extends CharacterBody2D
 @onready var animation_node = $AnimatedSprite2D
 
 @export var damage = 50
-@export var speed = 100
+@export var speed = 250
+
+var current_player
+var move_direction
 
 func _ready():
-	### fireball direction not implemented yet
-	velocity = speed * (Vector2(-1, 0)) #left for now
+	current_player = PartyStatsComponent.players[GlobalSettings.current_main_player]
+	move_direction = current_player.move_direction
+	
+	var nearest_enemy = find_nearest_enemy()
+	if nearest_enemy:
+		# Calculate the direction to the nearest enemy
+		var direction = (nearest_enemy.global_position - current_player.position).normalized()
+		velocity = direction * speed
+		print("Nearest enemy found at position: ", nearest_enemy.global_position)
+	elif move_direction != Vector2.ZERO: # shoot at player default facing direction
+		velocity = move_direction * speed
+	else: # shoot at player attack direction??
+		velocity = current_player.last_move_direction * speed 
 
 func _physics_process(_delta):
 	move_and_slide()
@@ -17,8 +31,19 @@ func _physics_process(_delta):
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
 
-
 func _on_checker_body_entered(body):
 	if body.is_in_group("enemies"):
 		body.take_damage(0, damage)
 	queue_free()
+
+func find_nearest_enemy():
+	var nearest_enemy = null
+	var shortest_distance = INF
+	var enemies = get_tree().get_nodes_in_group("enemies")
+	for enemy in enemies:
+		var distance = current_player.position.distance_to(enemy.global_position)
+		if distance < shortest_distance:
+			shortest_distance = distance
+			nearest_enemy = enemy
+		print("Enemy position: ", enemy.global_position, " Distance: ", distance)
+	return nearest_enemy
