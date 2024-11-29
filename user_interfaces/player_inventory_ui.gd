0541f7fe3f70160b5fd929d0edb9e66a4e3b1ef8
TomayHawk <gmtomayhawk@gmail.com>
2024-11-28 22:33:27 -0800

    Update Inventory

--- user_interfaces/player_inventory_ui.gd
+++ user_interfaces/player_inventory_ui.gd
@@ -1,12 +1,19 @@
 extends CanvasLayer
 
-var miscellaneous_buttons
-var miscellaneous_quantities
+var item_buttons
+var item_labels
+var item_quantities
+
+const item_names := {
+    0: "Potion",
+    1: "MAX Potion",
+    2: "Phoenix Burger"
+}
 
 func _ready():
     pass
     # for item_index in miscellaneous_buttons
 
-func update_items():
+func update_items(_inventory_index): # !#!# Consumables/Equipments/KeyItems
     for item_index in GlobalSettings.current_save["inventory"].size():
-        miscellaneous_quantities[item_index].text = str(GlobalSettings.current_save["inventory"][item_index]) ## ### ??????
+        item_quantities[item_index].text = str(GlobalSettings.current_save["inventory"][item_index]) ## ### ??????
