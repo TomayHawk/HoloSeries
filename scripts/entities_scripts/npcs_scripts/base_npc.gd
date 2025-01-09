368f5a82e756aa4b51c2a0891be5731c0e850cb0
TomayHawk <gmtomayhawk@gmail.com>
2025-01-08 23:15:07 -0800

    Restructured Npcs and Added World Interactable Objects

---
+++ scripts/entities_scripts/npcs_scripts/base_npc.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+var area_active := false
+
+@onready var npc_node := get_parent()
+
+func _ready():
+    set_process(false)
+    npc_node.set_process(false)
+    npc_node.add_to_group("npcs")
+    add_to_group("npcs")
+    
+func _input(_event):
+    # if in interactable area, interaction button just pressed, not in a dialogue and not in combat, start dialogue
+    if area_active and Input.is_action_just_pressed("interact") and !TextBox.textbox_container.is_visible() and !CombatEntitiesComponent.in_combat:
+        TextBox.text_queue += npc_node.dialogue;
+        TextBox.current_state = TextBox.Text.READY
+
+# triggered on npc entering/exiting player interaction area
+func interaction_area(check_bool):
+    area_active = check_bool
--- scripts/entities_scripts/npcs_scripts/npc_kobo.gd
+++ scripts/entities_scripts/npcs_scripts/npc_kobo.gd
@@ -1,18 +1,5 @@
-extends StaticBody2D
+extends Node2D
 
-var area_active := false
-
-func _ready():
-	set_process(false)
-
-func _input(_event):
-	# if in interactable area, interaction button just pressed, not in a dialogue and not in combat, start dialogue
-	if area_active and Input.is_action_just_pressed("interact") and !TextBox.textbox_container.is_visible() and !CombatEntitiesComponent.in_combat:
-		TextBox.text_queue += ["Never gonna give you up.",
-							   "Never gonna let you down.",
-							   "Never gonna run around and desert you!"]
-		TextBox.current_state = TextBox.Text.READY
-
-# triggered on npc entering/exiting player interaction area
-func interaction_area(check_bool):
-	area_active = check_bool
+const dialogue := ["Never gonna give you up.",
+				   "Never gonna let you down.",
+				   "Never gonna run around and desert you!"]
