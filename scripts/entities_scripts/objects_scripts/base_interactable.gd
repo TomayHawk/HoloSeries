e171fefe59a7fe76a60ad0e123ff31bb8497b0c0
TomayHawk <gmtomayhawk@gmail.com>
2025-01-18 22:23:43 -0800

    Update Physics and Navigation Layers

--- scripts/entities_scripts/objects_scripts/base_interactable.gd
+++ scripts/entities_scripts/objects_scripts/base_interactable.gd
@@ -5,7 +5,7 @@ var area_active := false
 @onready var object_node := get_parent()
 
 func _input(_event):
-	if area_active and Input.is_action_just_pressed("interact") and !TextBox.textbox_container.is_visible() and !CombatEntitiesComponent.in_combat:
+	if area_active and Input.is_action_just_pressed("interact") and TextBox.text_box_state == TextBox.TextBoxState.INACTIVE and !CombatEntitiesComponent.in_combat:
 		object_node.interact_object()
 
 func interaction_area(check_bool):
