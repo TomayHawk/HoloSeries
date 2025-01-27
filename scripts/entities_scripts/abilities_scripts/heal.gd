22ba64c5051a578848f63d85834db3f6e6745e4d
TomayHawk <gmtomayhawk@gmail.com>
2025-01-26 23:42:44 -0800

    Update Heal Script

--- scripts/entities_scripts/abilities_scripts/heal.gd
+++ scripts/entities_scripts/abilities_scripts/heal.gd
@@ -1,31 +1,34 @@
 extends Node2D
 
-@onready var caster_node := GlobalSettings.current_main_player_node
+## need to dynamically allocate caster in case of ally/enemy casts
+@onready var caster_node: Node = GlobalSettings.current_main_player_node
+@onready var caster_stats_node: Node = caster_node.player_stats_node
 
-const mana_cost := 8
-# heal 5%
-const heal_percentage := 0.05
-var heal_amount := 10.0
+var mana_cost: float = 4.0
+var heal_percentage: float = 0.05
 
 func _ready():
 	hide()
 
-	# request target entity
+	# request target entities
 	CombatEntitiesComponent.request_entities(self, "initiate_heal", 1, "players_alive")
-	
-	if CombatEntitiesComponent.entities_available.size() == 0:
-		queue_free()
-	# if alt is pressed, auto-aim player with lowest health
-	elif Input.is_action_pressed("alt"):
+
+	# if alt is pressed, target player with lowest health
+	if Input.is_action_pressed("alt"):
 		CombatEntitiesComponent.target_entity("health", caster_node)
 
 func initiate_heal(chosen_node):
+	## should add a variable for "player can cast spells"
+	# check caster mana sufficiency and 
+	if caster_stats_node.mana < mana_cost or !caster_stats_node.alive:
+		queue_free()
+		return
 	# check caster status and mana sufficiency
-	if caster_node.player_stats_node.mana > mana_cost and caster_node.player_stats_node.alive:
-		caster_node.player_stats_node.update_mana(-mana_cost)
 
-		# heal chosen node
-		heal_amount = CombatEntitiesComponent.magic_heal_calculator(chosen_node.player_stats_node.max_health * heal_percentage, caster_node.player_stats_node)
-		chosen_node.player_stats_node.update_health(heal_amount, [null], Vector2.ZERO, 0.0)
+	caster_stats_node.update_mana(-mana_cost)
+
+	# heal chosen node
+	var heal_amount: float = CombatEntitiesComponent.magic_heal_calculator(chosen_node.player_stats_node.max_health * heal_percentage, caster_stats_node)
+	chosen_node.player_stats_node.update_health(heal_amount, [null], Vector2.ZERO, 0.0)
 
 	queue_free()
